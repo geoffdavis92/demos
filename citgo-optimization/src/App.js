4f793c3ec7b72019fba97848702e740ef39197ed
Geoff Davis <gdavis@archermalmo.com>
2017-03-10 17:43:26 -0600

    get React Router working

--- citgo-optimization/src/App.js
+++ citgo-optimization/src/App.js
@@ -1,3 +1,21 @@
 // App
 import React, { Component } from 'react'
 import './styles/App.css'
+
+import {
+	Router,
+	browserHistory,
+	Route
+} from 'react-router'
+
+import Home from './views/Home'
+
+export default class App extends Component {
+	render() {
+		return (
+			<Router history={browserHistory}>
+				<Route exact path='/' component={Home}/>
+			</Router>
+		)
+	}
+}
---
+++ citgo-optimization/src/views/Home.js
@@ -0,0 +1,9 @@
+// Home
+import React, { Component } from 'react'
+import '../styles/Home.css'
+
+export default class Home extends Component {
+	render() {
+		return <p>Home</p>
+	}
+}
